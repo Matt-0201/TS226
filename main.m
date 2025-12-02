5d4f2ff78a8e81cb73fa5464757c48ebf2fb95e5
TRAVERSINO Eoghan <eoghan.traversino@bordeaux-inp.fr>
2025-12-02 15:48:31 +0100

    Supperposition sur une seule et meme figure des affichages des differents codes convolutifs

--- main.m
+++ main.m
@@ -43,16 +43,47 @@ M = 2;   % Modulation BPSK <=> 2 symboles
 % feedback = 7;
 
 %Code convutif (1,13/17)
-m = 3;
-contrainte = m + 1;
-polynome = [15, 17];
-feedback = 15;
+% m = 3;
+% contrainte = m + 1;
+% polynome = [15, 17];
+% feedback = 15;
 
 %% Codes convolutifs recusifs
-trellis = poly2trellis(contrainte, polynome, feedback);%Definition trelli code convolutif
+%trellis = poly2trellis(contrainte, polynome, feedback);%Definition trelli code convolutif
+
+%% Execution simultanée des codes convolutifs
+% polynomes = {[2 3], [5 7], [13 15], [133 171]};
+% noms_codages = {'(2,3)_8','(5,7)_8','(13,15)_8','(133,171)_8'};
+% contraintes = [2 3 4 7];
+% 
+% colors = {'r','b','g','k'}; % couleurs pour les codes
+% 
+% trellises = cell(size(polynomes));
+% for k = 1:length(polynomes)
+%     trellises{k} = poly2trellis(contraintes(k), polynomes{k});
+% end
+
+%% Execution simultanée des codes convolutifs récusifs
+polynomes = {[5 7], [5 7], [13 15], [13 15]};
+feedback = {[], 7, [], 15};
+noms_codages = {'(5,7)_8','(1,5/7)_8','(13,15)_8','(1,13/15)_8'};
+contraintes = [3 3 4 4];
+
+colors = {'r','b','g','k'}; % couleurs pour les codes
+
+trellises = cell(size(polynomes));
+for k = 1:length(polynomes)
+    if mod(k, 2) == 0
+        trellises{k} = poly2trellis(contraintes(k), polynomes{k},feedback{k});
+    else
+        trellises{k} = poly2trellis(contraintes(k), polynomes{k});
+    end
+    %trellises{k} = poly2trellis(contraintes(k), polynomes{k},feedback{k});
+end
+
 
 EbN0dB_min  = 0; % Minimum de EbN0
-EbN0dB_max  = 10; % Maximum de EbN0
+EbN0dB_max  = 8; % Maximum de EbN0
 EbN0dB_step = 1;% Pas de EbN0
 
 nbrErreur  = 100;  % Nombre d'erreurs à observer avant de calculer un BER
@@ -95,89 +126,105 @@ fprintf(line      );
 
 
 %% Simulation
-for iSNR = 1:length(EbN0dB)
-    reverseStr = ''; % Pour affichage en console stat_erreur
+for indice_code = 1:length(trellises)
+    trellis = trellises{indice_code};
+    fprintf('Simulation pour le code %s\n', noms_codages{indice_code});
     
-    pqtNbr  = 0; % Nombre de paquets envoyés
-    bitErr  = 0; % Nombre de bits faux
-    pqtErr  = 0; % Nombre de paquets faux
-    T_rx = 0;
-    T_tx = 0;
-    general_tic = tic;
-    while (bitErr < nbrErreur && pqtNbr*K < nbrBitMax)
-        pqtNbr = pqtNbr + 1;
+    %Initialisation vecteurs de resultats
+    TEP = zeros(1,length(EbN0dB));
+    TEB = zeros(1,length(EbN0dB));
+    
+    %Créaation des courbes
+    hTEB = semilogy(EbN0dB, TEB, 'LineWidth',1.5,'XDataSource','EbN0dB','YDataSource','TEB');
+    hTEP = semilogy(EbN0dB, TEP, 'LineWidth',1.5,'XDataSource','EbN0dB','YDataSource','TEP');
+    
+    %Definition couleur + titre
+    set(hTEB, 'Color', colors{indice_code}, 'DisplayName', ['TEB ' noms_codages{indice_code}]);
+    set(hTEP, 'Color', colors{indice_code}, 'DisplayName', ['TEP ' noms_codages{indice_code}]);
+    for iSNR = 1:length(EbN0dB)
+        reverseStr = ''; % Pour affichage en console stat_erreur
         
-        %% Emetteur
-        tx_tic  = tic;                 % Mesure du débit d'encodage
-        u       = randi([0,1],K,1);    % Génération du message aléatoire
-        %c       = u;                   % Encodage
-        c = cc_encode(u, trellis);     %Encodage code convolutif
-        x       = 1-2*c;               % Modulation QPSK
-        T_tx    = T_tx+toc(tx_tic);    % Mesure du débit d'encodage
-        debitTX = pqtNbr*K/8/T_tx/1e6;
-        
-        %% Canal
-        z = sqrt(sigmaz2(iSNR)) * randn(size(x)); % Génération du bruit blanc gaussien
-        y = x + z;                          % Ajout du bruit blanc gaussien
-        
-        %% Recepteur
-        rx_tic = tic;                  % Mesure du débit de décodage
-        Lc      = 2*y/sigmaz2(iSNR);   % Démodulation (retourne des LLRs)
-        %u_rec   = double(Lc(1:K) < 0); % Message reçu
-        u_rec = viterbi_decode(y,trellis); %L'utilisation de LC à la place de y serait plus optimale
-        
-        %Supression de la fermeture
-        %c = c(1:K);
-
-        BE      = sum(u(:) ~= u_rec(:)); % Nombre de bits faux sur cette trame
-        bitErr  = bitErr + BE;
-        pqtErr  = pqtErr + double(BE>0);
-        T_rx    = T_rx + toc(rx_tic);  % Mesure du débit de décodage
-        debitRX = pqtNbr*K/8/T_rx/1e6;
-        %% Affichage du résultat
-        if mod(pqtNbr,100) == 1
-            pct1 = bitErr/nbrErreur;
-            pct2 = pqtNbr*K/nbrBitMax;
-            pct  = max(pct1, pct2);
+        pqtNbr  = 0; % Nombre de paquets envoyés
+        bitErr  = 0; % Nombre de bits faux
+        pqtErr  = 0; % Nombre de paquets faux
+        T_rx = 0;
+        T_tx = 0;
+        general_tic = tic;
+        while (bitErr < nbrErreur && pqtNbr*K < nbrBitMax)
+            pqtNbr = pqtNbr + 1;
             
-            display_str = sprintf(msgFormat,...
-                EbN0dB(iSNR),               ... % EbN0 en dB
-                pqtNbr*K,                   ... % Nombre de bits envoyés
-                bitErr,                     ... % Nombre d'erreurs observées
-                pqtErr,                     ... % Nombre d'erreurs observées
-                bitErr/(pqtNbr*K),          ... % TEB
-                pqtErr/pqtNbr,              ... % TEP
-                debitTX,                    ... % Débit d'encodage
-                debitRX,                    ... % Débit de décodage
-                toc(general_tic)/pct*(1-pct)); % Temps restant
-            lr = length(reverseStr);
-            msg_sz =  fprintf([reverseStr, display_str]);
-            reverseStr = repmat(sprintf('\b'), 1, msg_sz-lr);
+            %% Emetteur
+            tx_tic  = tic;                 % Mesure du débit d'encodage
+            u       = randi([0,1],K,1);    % Génération du message aléatoire
+            %c       = u;                   % Encodage
+            c = cc_encode(u, trellis);     %Encodage code convolutif
+            x       = 1-2*c;               % Modulation QPSK
+            T_tx    = T_tx+toc(tx_tic);    % Mesure du débit d'encodage
+            debitTX = pqtNbr*K/8/T_tx/1e6;
             
-            TEB(iSNR) = bitErr/(pqtNbr*K);
-            TEP(iSNR) = pqtErr/pqtNbr;
-            refreshdata(hTEB);
-            refreshdata(hTEP);
+            %% Canal
+            z = sqrt(sigmaz2(iSNR)) * randn(size(x)); % Génération du bruit blanc gaussien
+            y = x + z;                          % Ajout du bruit blanc gaussien
+            
+            %% Recepteur
+            rx_tic = tic;                  % Mesure du débit de décodage
+            Lc      = 2*y/sigmaz2(iSNR);   % Démodulation (retourne des LLRs)
+            %u_rec   = double(Lc(1:K) < 0); % Message reçu
+            u_rec = viterbi_decode(Lc,trellis); %L'utilisation de LC à la place de y serait plus optimale
+            
+            %Supression de la fermeture
+            %c = c(1:K);
+    
+            BE      = sum(u(:) ~= u_rec(:)); % Nombre de bits faux sur cette trame
+            bitErr  = bitErr + BE;
+            pqtErr  = pqtErr + double(BE>0);
+            T_rx    = T_rx + toc(rx_tic);  % Mesure du débit de décodage
+            debitRX = pqtNbr*K/8/T_rx/1e6;
+            %% Affichage du résultat
+            if mod(pqtNbr,100) == 1
+                pct1 = bitErr/nbrErreur;
+                pct2 = pqtNbr*K/nbrBitMax;
+                pct  = max(pct1, pct2);
+                
+                display_str = sprintf(msgFormat,...
+                    EbN0dB(iSNR),               ... % EbN0 en dB
+                    pqtNbr*K,                   ... % Nombre de bits envoyés
+                    bitErr,                     ... % Nombre d'erreurs observées
+                    pqtErr,                     ... % Nombre d'erreurs observées
+                    bitErr/(pqtNbr*K),          ... % TEB
+                    pqtErr/pqtNbr,              ... % TEP
+                    debitTX,                    ... % Débit d'encodage
+                    debitRX,                    ... % Débit de décodage
+                    toc(general_tic)/pct*(1-pct)); % Temps restant
+                lr = length(reverseStr);
+                msg_sz =  fprintf([reverseStr, display_str]);
+                reverseStr = repmat(sprintf('\b'), 1, msg_sz-lr);
+                
+                TEB(iSNR) = bitErr/(pqtNbr*K);
+                TEP(iSNR) = pqtErr/pqtNbr;
+                refreshdata(hTEB);
+                refreshdata(hTEP);
+            end
+            
+        end
+        
+        display_str = sprintf(msgFormat, EbN0dB(iSNR), pqtNbr*K, bitErr, pqtErr, bitErr/(pqtNbr*K), pqtErr/pqtNbr, debitTX, debitRX, 0);
+        fprintf(reverseStr);
+        msg_sz =  fprintf(display_str);
+        reverseStr = repmat(sprintf('\b'), 1, msg_sz);
+        
+        TEB(iSNR) = bitErr/(pqtNbr*K);
+        TEP(iSNR) = pqtErr/pqtNbr;
+        refreshdata(hTEB);
+        refreshdata(hTEP);
+        drawnow limitrate
+        
+        if TEB(iSNR) < TEBMin
+            break
         end
         
     end
-    
-    display_str = sprintf(msgFormat, EbN0dB(iSNR), pqtNbr*K, bitErr, pqtErr, bitErr/(pqtNbr*K), pqtErr/pqtNbr, debitTX, debitRX, 0);
-    fprintf(reverseStr);
-    msg_sz =  fprintf(display_str);
-    reverseStr = repmat(sprintf('\b'), 1, msg_sz);
-    
-    TEB(iSNR) = bitErr/(pqtNbr*K);
-    TEP(iSNR) = pqtErr/pqtNbr;
-    refreshdata(hTEB);
-    refreshdata(hTEP);
-    drawnow limitrate
-    
-    if TEB(iSNR) < TEBMin
-        break
-    end
-    
+    fprintf(line      );
 end
-fprintf(line      );
 %%
 save('NC.mat','EbN0dB','TEB', 'TEP', 'R', 'K', 'N', 'Pb_u', 'Pe_u')
